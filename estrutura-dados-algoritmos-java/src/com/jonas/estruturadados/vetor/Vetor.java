507ce8d00467aff3e06e0a22996b1813e99b1a47
Jonas Lucena <jonaslucena@outlook.com>
2020-06-14 12:04:29 -0300

    implementacao metodo toString e metodo getTamanho

--- estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
@@ -32,4 +32,28 @@ public class Vetor {
 		} 
 		return false;
 	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			sb.append(this.elementos[i]);
+			sb.append(", ");
+		}
+		
+		if (this.tamanho > 0) {
+			sb.append(this.elementos[this.tamanho - 1]);
+		}
+		
+		sb.append("]");
+		
+		return sb.toString();
+	}
 }
