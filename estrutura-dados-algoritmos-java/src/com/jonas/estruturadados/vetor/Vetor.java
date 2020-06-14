d9a772ef61f4f317b22a46c572a726332b224747
Jonas Lucena <jonaslucena@outlook.com>
2020-06-14 12:02:07 -0300

    implementacao metodo adicionar

--- estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
@@ -3,8 +3,33 @@ package com.jonas.estruturadados.vetor;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanho;
 
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
+	}
+
+	/*
+	 * public void adicionar(String elemento) { for (int i = 0; i <
+	 * this.elementos.length; i++) { if (this.elementos[i] == null) {
+	 * this.elementos[i] = elemento; break; } } }
+	 */
+
+	/*
+	 * public void adicionar(String elemento) throws Exception { if(this.tamanho <
+	 * this.elementos.length) { this.elementos[this.tamanho] = elemento;
+	 * this.tamanho++; } else { throw new
+	 * Exception("Vetor já está cheio, não é possível adicionar mais elementos!"); }
+	 * }
+	 */
+
+	public boolean adicionar(String elemento) {
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		} 
+		return false;
 	}
 }
---
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,24 @@
+package com.jonas.estruturadados.vetor.teste;
+
+import com.jonas.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		/*try {
+			vetor.adicionar("elemento 1");
+			vetor.adicionar("elemento 2");
+			vetor.adicionar("elemento 3");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		vetor.adicionar("elemento 1");
+		vetor.adicionar("elemento 2");
+		vetor.adicionar("elemento 3");
+	}
+}
