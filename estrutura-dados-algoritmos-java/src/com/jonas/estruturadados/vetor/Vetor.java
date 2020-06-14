185c463b1119ba4df74970d66569b5a9ec18ac1e
Jonas Lucena <jonaslucena@outlook.com>
2020-06-14 11:59:50 -0300

    implementacao do construtor

--- estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/Vetor.java
@@ -2,4 +2,9 @@ package com.jonas.estruturadados.vetor;
 
 public class Vetor {
 
+	private String[] elementos;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
 }
---
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,12 @@
+package com.jonas.estruturadados.vetor.teste;
+
+import com.jonas.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		// vetor com capacidade para 100 elementos
+		Vetor vetor = new Vetor(5);
+	}
+}
