ba5cc1c2efa0f07e993c74753d31fc3a7fb997e7
Jonas Lucena <jonaslucena@outlook.com>
2020-06-21 12:27:23 -0300

    implementacao do metodo empilhar

--- estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/base/EstruturaEstatica.java
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/base/EstruturaEstatica.java
@@ -2,8 +2,8 @@ package com.jonas.estruturadados.base;
 
 public class EstruturaEstatica<T> {
 
-	private T[] elementos;
-	private int tamanho;
+	protected T[] elementos;
+	protected int tamanho;
 
 	public EstruturaEstatica(int capacidade) {
 		this.elementos = (T[]) new Object[capacidade];
---
+++ estrutura-dados-algoritmos-java/src/com/jonas/estruturadados/pilha/teste/Aula14.java
@@ -0,0 +1,18 @@
+package com.jonas.estruturadados.pilha.teste;
+
+import com.jonas.estruturadados.pilha.Pilha;
+
+public class Aula14 {
+
+	public static void main(String[] args) {
+
+		Pilha<Integer> pilha = new Pilha<Integer>();
+		
+		for ( int i = 1; i <= 10; i++) {
+			pilha.empilhar(i);
+		}
+		
+		System.out.println(pilha);
+		System.out.println(pilha.getTamanho());
+	}
+}
